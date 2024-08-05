87cae55ebd8c299a240b5c9d6f4323b2b8d71c12
kq-al <154840936+kq-al@users.noreply.github.com>
2024-08-05 15:08:04 -0400

    final

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLi = document.querySelectorAll('header .nav-links li a');
+    function scrollWithOffset(event) {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const headerOffset = document.querySelector('header').offsetHeight;
+        const elementPosition = targetElement.offsetTop;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
+
+    navLi.forEach(a => {
+        a.addEventListener('click', scrollWithOffset);
+    });
+
+    window.onscroll = () => {
+        let current = "";
+        const headerOffset = document.querySelector('header').offsetHeight;
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - headerOffset;
+            if (pageYOffset >= sectionTop - 10) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLi.forEach(a => {
+            a.classList.remove('active');
+            if (a.getAttribute('href').substring(1) === current) {
+                a.classList.add('active');
+            }
+        });
+    };
+});
